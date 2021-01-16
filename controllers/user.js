9e2a963f1168741a2bd63637809d0824d7f90d23
Bayurzx <bayurzx@gmail.com>
2021-01-16 01:35:22 +0100

    Fixed some issues

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,4 @@
 const User = require('../models/user');
-const {Order} = require('../models/order');
 
 exports.userById = (req, res, next, id) => {
   User.findById(id).exec((error, user) => {
@@ -39,46 +38,4 @@ exports.update = (req, res) => {
       return res.json(user);
     }
   )
-}
-
-exports.addOrderToUserHistory = (req, res, next) => {
-  let history = []
-
-  req.body.order.products.forEach((item) => {
-    history.push({
-      _id: item._id,
-      name: item.name,
-      description: item.description,
-      category: item.category,
-      quantity: item.quantity,
-      transaction_id: req.body.order.transaction_id,
-      amount: req.body.order.amount
-    })
-  })
-
-  User.findOneAndUpdate(
-    {_id: req.profile._id},
-    {$push: {history: history}},
-    {new: true},
-    (error, data) => {
-      if (error) {
-        res.status(400).json({
-          error: "Could not update user purchase history"
-        });
-      }
-      next();
-    }
-  );
-}
-
-exports.purchaseHistory = (req, res) => {
-  Order.find({user: req.profile._id})
-  .populate('user', '_id name')
-  .sort('-created')
-  .exec((error, orders) => {
-    if (error) {
-      res.status(400).json({ error: error });
-    }
-    res.json(orders);
-  })
 }
